9e2bfa02ba012075c41d7c776d6e553c2bb987ae
Keegan Parker <KeeganDParker@gmail.com>
2015-10-17 09:34:23 -0600

    Vote mockup

--- api-backend/app/views.py
+++ api-backend/app/views.py
@@ -2,12 +2,25 @@ from app import app, db
 from flask import request, abort, jsonify
 from models import *
 
+def normalize_url(url):
+	url = url.lower()
+	prefix_index = url.find('://')
+	if prefix_index > -1:
+		url = url[prefix_index+3:]
+	param_index = url.find('?')
+	if param_index > -1:
+		url = url[:param_index]
+	octothorpe_index = url.find('#')
+	if octothorpe_index > -1:
+		url = url[:octothorpe_index]
+	return url
+
 def make_json_response(json, status_code=200):
 	resp = jsonify(json)
 	resp.status_code = status_code
 	return resp
 
-@app.route('/users', methods = ['POST'])
+@app.route('/api/users', methods = ['POST'])
 def register_user():
 	username = request.json.get('username')
 	password = request.json.get('password')
@@ -25,3 +38,22 @@ def register_user():
 	db.session.commit()
 
 	return make_json_response({'data':{'succeeded': True, 'message': "User created successfully"}}, 201)
+
+@app.route('/api/vote', methods = ['POST'])
+# Force authentication
+def vote():
+	url = normalize_url(request.json.get('url'))
+	user = None
+	# user = get user somehow
+	upvote = request.json.get('upvote')
+	site = Site.query.filter_by(url=url).first()
+	if site is None:
+		new_site = Site(url=url)
+		db.session.add(new_site)
+		db.session.commit()
+		site = new_site
+	vote = Vote(site.id, user.id, upvote)
+	db.session.add(vote)
+	db.session.commmit()
+
+	return make_json_response({'data':{'succeeded': True, 'message': "Vote created succesfully"}}, 201)
