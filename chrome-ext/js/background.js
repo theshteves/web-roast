13611aa144e94d4c48ab8f972b2fff0fdf196757
Keegan Parker <KeeganDParker@gmail.com>
2015-10-17 20:50:33 -0400

    Extension: Fix xhr headers

--- chrome-ext/js/background.js
+++ chrome-ext/js/background.js
@@ -3,7 +3,8 @@ logged_in = false;
 function register(username, email, password) {
     console.log("Sending register request");
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://159.203.97.105:5000/api/users", true);
+    xhr.open("POST", "http://webroast.club/api/users", true);
+    xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             console.log(xhr.responseText);
@@ -13,13 +14,14 @@ function register(username, email, password) {
         username: username,
         email: email,
         password: password
-    });
+    }));
 }
 
 function login(username, password) {
-    console.log("Sending login request");
+    console.log("Sending lpogin request");
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://159.203.97.105:5000/api/login", true);
+    xhr.open("POST", "http://webroast.club/api/login", true);
+    xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             console.log(xhr.responseText);
@@ -28,16 +30,17 @@ function login(username, password) {
     xhr.send(JSON.stringify({
         username: username,
         password: password
-    });
+    }));
 }
 
 function check_logged_in() {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://159.203.97.105:5000/api/check", true);
+    xhr.open("POST", "http://webroast.club/api/check", true);
+    xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             console.log(xhr.responseText);
         }
     }
-    xhr.send();
+    xhr.send('{}');
 }
